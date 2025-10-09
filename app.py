9f84d30365864667ef55305262fc6d4fa4de309d
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2025-10-08 18:28:29 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    
+    return render_template("index.html")
+
+@app.route("/ani")
+def animales():
+    
+    return render_template("animales.html")
+
+@app.route("/veh")
+def vehiculos():
+    
+    return render_template("vehiculos.html")
+
+@app.route("/mar")
+def maravillas():
+    
+    return render_template("maravillas.html")
+
+@app.route("/ace")
+def acercas():
+    
+    return render_template("acerca.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
